0652925db54792951f2d331cded29108fca2d742
Arsen Vynnytskyy <arsen@Arsen-MacBook-Pro.local>
2021-01-27 16:37:52 +0200

    Update

--- lesson4/object.rb
+++ lesson4/object.rb
@@ -1,8 +1,21 @@
 puts "string".length
 p "string"
-a = true
-if a
-	puts "a - true"
+a = 172.168
+b = 31.514
+if a > b
+	puts "a bigger than b. a = #{a}"
 else
-	puts "a - false"
+	puts "a less than b. b = #{b}"
+end
+
+puts "First number: #{a.to_i}"
+puts "Second number: #{b.to_i}"
+puts "Average of first and second numbers is: #{((a+b)/2).to_i}"
+
+test = 7823823868389327831
+
+if test %2 == 0
+	puts "Even."
+else
+	puts "Not even."
 end
