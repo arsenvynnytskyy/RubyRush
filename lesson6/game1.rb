8ea88f3b19e305247ce2162bef0618c13a34324b
Arsen Vynnytskyy <arsen@Arsen-MacBook-Pro.local>
2021-01-28 15:33:12 +0200

    Update home tasks in lesson 6

--- lesson6/game1.rb
+++ lesson6/game1.rb
@@ -1,6 +1,5 @@
 puts "Game Stone Scissors Paper"
 puts ""
-
 puts "Take your choise:"
 variants = ["1. Stone", "2. Scissors", "3. Paper"]
 puts variants
@@ -27,14 +26,10 @@ if user_choice == computer_random_choice
 	puts "DRAW. Try again."
 elsif user_choice == 0 && computer_random_choice == 1
 	puts "You WON!!!"
-elsif user_choice == 0 && computer_random_choice == 2
-	puts "You LOOSE!!!"
-elsif user_choice == 1 && computer_random_choice == 0
-	puts "You LOOSE!!!"
 elsif user_choice == 1 && computer_random_choice == 2
 	puts "You WON!!!"
 elsif user_choice == 2 && computer_random_choice == 0
 	puts "You WON!!!"
-elsif user_choice == 2 && computer_random_choice == 1
+else
 	puts "You LOOSE!!!"
 end
