34ae35213bb269e58bbdb0b5aff6c95d2d85f788
Arsen Vynnytskyy <arsen@Arsen-MacBook-Pro.local>
2021-02-08 13:52:26 +0200

    Some fixes in lesson8

--- lesson8/argv.rb
+++ lesson8/argv.rb
@@ -1,8 +1,9 @@
-
 argument = ARGV[0]
 
-if (argument == "looser")
-	puts "Asshole!"
+if argument == "looser"
+	puts "You are #{argument}! =)"
+elsif argument == nil
+	abort "Need any argument!"
 else
-	puts "Hello!"
+	puts "Hello #{argument}!"
 end
