00e86f49fa1359d7bfefa343b2ec8a6d3f82f769
Arsen Vynnytskyy <arsen@Arsen-MacBook-Pro.local>
2021-02-09 14:05:12 +0200

    Fixes in lesson9.

--- lesson9/methods1.rb
+++ lesson9/methods1.rb
@@ -1,19 +1,19 @@
 def sklonenie(number, krokodil, krokodila, krokodilov)
 	ostatok = number % 10
 
-	if (number == nil || !number.is_a?(Numeric))
+	if number == nil || !number.is_a?(Numeric)
 		number = 0
 	end
 
-	if (ostatok == 1)
+	if ostatok == 1
 		return krokodil	
 	end
 
-	if (ostatok >= 2 && ostatok <= 4)
+	if ostatok >= 2 && ostatok <= 4
 		return krokodila
 	end
 
-	if (ostatok > 4 || ostatok == 0)
+	if ostatok > 4 || ostatok == 0
 		return krokodilov
 	end
 end
