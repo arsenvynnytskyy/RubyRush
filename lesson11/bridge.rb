6614bc8c36f6f08c9cdb9e648911996cca9c0bc0
Arsen Vynnytskyy <arsen@Arsen-MacBook-Pro.local>
2021-03-04 23:40:11 +0200

    add lesson11

---
+++ lesson11/bridge.rb
@@ -0,0 +1,11 @@
+class Bridge
+
+	def initialize
+		puts "Bridge is created."
+	end
+
+	def open
+		puts "Bridge is open. You can drive."
+	end
+	
+end
---
+++ lesson11/doroga.rb
@@ -0,0 +1,19 @@
+require "./bridge.rb"
+
+puts "Go!"
+
+sleep 1
+
+puts "Gangerous! River!"
+
+sleep 1
+
+bridge = Bridge.new
+
+sleep 1
+
+bridge.open
+
+sleep 1
+
+puts "Go ahead!"
