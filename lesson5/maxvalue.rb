81fd6b6367ab18fd09c44ec8f728e3ad34617284
Arsen Vynnytskyy <arsen@Arsen-MacBook-Pro.local>
2021-06-20 20:29:13 +0300

    Refactoring in lesson5

--- lesson5/maxvalue.rb
+++ lesson5/maxvalue.rb
@@ -1,11 +1,12 @@
-a = 5
-b = 7
-puts a, b
+a = 172.169
+b = 31.514
+puts a
+puts b
 
 if a > b
 	puts "Max value a=#{a}"
 	puts "Max value a=" + a.to_s
-elsif a==b
+elsif a == b
 	puts "Variables a and b - are equal"
 else
 	puts "Max value b=#{b}"
